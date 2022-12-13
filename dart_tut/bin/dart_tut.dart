5f4fa01813d1817f9af116583824dcb06c43e684
ruahman <dego_vila@yahoo.com>
2022-12-12 21:57:53 -0400

    simple dart tut

---
+++ dart_tut/bin/dart_tut.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tut/dart_tut.dart' as dart_tut;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tut.calculate()}!');
+}
---
+++ dart_tut/test/dart_tut_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tut/dart_tut.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
