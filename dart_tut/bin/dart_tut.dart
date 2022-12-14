6dfa522a072ea7d620fc529f99367dc07c0a7973
ruahman <dego_vila@yahoo.com>
2022-12-13 20:17:53 -0400

    dart variables

--- dart_tut/bin/dart_tut.dart
+++ dart_tut/bin/dart_tut.dart
@@ -1,5 +1,7 @@
 import 'package:dart_tut/dart_tut.dart' as dart_tut;
+import 'package:dart_tut/variables.dart' as variables;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_tut.calculate()}!');
+  variables.demo();
 }
---
+++ dart_tut/lib/variables.dart
@@ -0,0 +1,11 @@
+void demo() {
+  print("--- variables ---");
+  int x = 23;
+  String name = "Diego";
+  double number = 2.2;
+  bool student = true;
+  var male = true;
+  const name2 = "sara";
+
+  print("$x, $name, $number, $student, $male, $name2");
+}
